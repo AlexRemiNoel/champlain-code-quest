e0eb24d66ed9aab4cd9e28ff50fd00d044d11cb0
Anvar <73323166+anvsult@users.noreply.github.com>
2025-11-11 10:34:35 -0500

    Update schedule timings in ScheduleModal

--- src/components/modals/ScheduleModal.jsx
+++ src/components/modals/ScheduleModal.jsx
@@ -9,19 +9,35 @@ export default function ScheduleModal({ open, onClose }) {
           <tbody>
             <tr>
               <td>Opening Ceremony:</td>
-              <td>8:00 AM - 9:00 AM</td>
+              <td>8:00 AM - 8:30 AM</td>
             </tr>
             <tr>
               <td>Coding Time:</td>
-              <td>8:30 AM - 8:00 PM</td>
+              <td>8:30 AM - 6:00 PM</td>
+            </tr>
+            <tr>
+              <td>Workshop on AI Image Recognition:</td>
+              <td>9:00 AM - 9:45 AM</td>
+            </tr>
+            <tr>
+              <td>Workshop on Tech Job Market:</td>
+              <td>11:00 AM - 11:45 AM</td>
             </tr>
             <tr>
               <td>Lunch Break:</td>
               <td>12:00 PM - 13:00 PM</td>
             </tr>
             <tr>
+              <td>Workshop on Deployment:</td>
+              <td>03:00 PM - 3:45 PM</td>
+            </tr>
+            <tr>
+              <td>Judging:</td>
+              <td>6:30 PM - 7:30 PM</td>
+            </tr>
+            <tr>
               <td>Closing Ceremony:</td>
-              <td>6:30 PM - 8:00 PM</td>
+              <td>7:30 PM - 8:00 PM</td>
             </tr>
           </tbody>
         </table>
